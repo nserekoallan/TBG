317c4b7828fc1311982ff7063bf9e627611b05d1
Allan Nsereko <ansereko@dusupay.com>
2025-08-15 08:31:29 +0300

    Fix blank page issue on GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/TBG">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/campaigns" element={<CampaignsPage />} />
